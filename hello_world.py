9e1d469300ceba7cc0a85f8575d8e6713ddc4f5d
miqueias <miqueiasricardobueno@gmail.com>
2026-07-17 13:49:01 -0300

    Hello beecrowd!

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,5 @@
-# """Bem-vindo ao beecrowd!
-# O seu primeiro programa em qualquer linguagem de programação normalmente é o "Hello World!". Neste primeiro problema tudo o que você precisa fazer é imprimir esta mensagem na tela.
+# """Bem-vindo ao bee-crowd!
+# O seu primeiro programa em qualquer linguagem de programação é normalmente o "Hello World!". Neste primeiro problema tudo o que você precisa fazer é imprimir esta mensagem na tela.
 # Entrada
 # Este problema não possui nenhuma entrada.
 # Saída
