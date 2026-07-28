705c65ed9682b478d6d8b67d63a3410dd4babf38
miqueias <miqueiasricardobueno@gmail.com>
2026-07-28 08:59:28 -0300

    atualização do programa cedulas

--- cedulas.py
+++ cedulas.py
@@ -9,18 +9,15 @@
 # mensagem: “Presentation Error”.
 
 N = int(input())
-n100 = N // 100
-n50 = (N % 100) // 50
-n20 = (N % 100 % 50) // 20
-n10 = (N % 100 % 50 % 20) // 10
-n5 = (N % 100 % 50 % 20 % 10) // 5
-n2 = (N % 100 % 50 % 20 % 10 % 5) // 2
-n1 = (N % 100 % 50 % 20 % 10 % 5 % 2) // 1
+valor_notas_divisores = [100, 50, 20, 10, 5, 2, 1]
+resto = N
+quantidade_notas = []
+for divisior in valor_notas_divisores:
+    quantidade = resto // divisior
+    resto %= divisior
+    quantidade_notas.append(quantidade)
 print(N)
-print(f"{n100} nota(s) de R$ 100,00")
-print(f"{n50} nota(s) de R$ 50,00")
-print(f'{n20} nota(s) de R$ 20,00')
-print(f"{n10} nota(s) de R$ 10,00")
-print(f"{n5} nota(s) de R$ 5,00")
-print(f"{n2} nota(s) de R$ 2,00")
-print(f"{n1} nota(s) de R$ 1,00")
+cont = 0
+for quantidade_cedulas in quantidade_notas: 
+    print(f"{quantidade_cedulas} nota(s) de R$ {valor_notas_divisores[cont]:.2f}")
+    cont += 1
