c349997301189955a75ed5efc62fe8fa0235ebf8
miqueias <miqueiasricardobueno@gmail.com>
2026-08-21 14:20:27 -0300

    Exercício bee crowd 1096, sequencia IJ 2.

---
+++ problemas_iniciantes/sequencia_I_J_2.py
@@ -0,0 +1,13 @@
+# Você deve fazer um programa que apresente a sequência conforme o exemplo abaixo.
+# Entrada
+# Não há nenhuma entrada neste problema.
+# Saída
+# Imprima a sequência conforme exemplo abaixo
+
+i = 1
+while i <= 9:
+    j = 7
+    while j >= 5:
+        print(f"I={i} J={j}")
+        j -= 1
+    i += 2
