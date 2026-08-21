2c56e2620486333212425a779fa9614ad92708a3
miqueias <miqueiasricardobueno@gmail.com>
2026-08-21 15:00:28 -0300

    Exercício bee crowd 1076, sequencia IJ 3.

---
+++ problemas_iniciantes/sequencia_I_J_3.py
@@ -0,0 +1,41 @@
+# Você deve fazer um programa que apresente a sequência conforme o exemplo abaixo.
+# Entrada
+# Não há nenhuma entrada neste problema.
+# Saída
+# Imprima a sequência conforme exemplo abaixo.
+
+i = 1
+while i <= 1:
+    j = 7
+    while j >= 5:
+        print(f"I={i} J={j}")
+        j -= 1
+    i += 1
+i = 3
+while i <= 3:
+    j = 9
+    while j >= 7:
+        print(f"I={i} J={j}")
+        j -= 1
+    i += 1
+i = 5
+while i <= 5:
+    j = 11
+    while j >= 9:
+        print(f"I={i} J={j}")
+        j -= 1
+    i += 1
+i = 7
+while i <= 7:
+    j = 13
+    while j >= 11:
+        print(f"I={i} J={j}")
+        j -= 1
+    i += 1
+i = 9
+while i <= 9:
+    j = 15
+    while j >= 13:
+        print(f"I={i} J={j}")
+        j -= 1
+    i += 1
