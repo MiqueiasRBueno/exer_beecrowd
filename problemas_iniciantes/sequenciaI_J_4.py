5f92c8a4e0184224837d959d5f11929d908330f9
miqueias <miqueiasricardobueno@gmail.com>
2026-08-27 13:57:03 -0300

    Exercício bee crowd 1098, sequencia IJ 4.

---
+++ problemas_iniciantes/sequenciaI_J_4.py
@@ -0,0 +1,50 @@
+#Você deve fazer um programa que apresente a sequencia conforme o exemplo abaixo.
+# Entrada
+# Não há nenhuma entrada neste problema.
+# Saída
+# Imprima a sequencia conforme exemplo abaixo.
+
+for i in range(0, 1):
+    for j in range(1, 4):
+        print(f"I={i} J={j}")
+        j += 1
+for i in range(0, 1):
+    for j in range(1, 4):
+        print(f"I={i + 0.2} J={j + 0.2}")
+        j += 1
+for i in range(0, 1):
+    for j in range(1, 4):
+        print(f"I={i + 0.4} J={j + 0.4}")
+        j += 1
+for i in range(0, 1):
+    for j in range(1, 4):
+        print(f"I={i + 0.6} J={j + 0.6}")
+        j += 1
+for i in range(0, 1):
+    for j in range(1, 4):
+        print(f"I={i + 0.8} J={j + 0.8}")
+        j += 1
+for i in range(1, 2):
+    for j in range(2, 5):
+        print(f"I={i} J={j}")
+        j += 1
+for i in range(1, 2):
+    for j in range(2, 5):
+        print(f"I={i + 0.2} J={j + 0.2}")
+        j += 1
+for i in range(1, 2):
+    for j in range(2, 5):
+        print(f"I={i + 0.4} J={j + 0.4}")
+        j += 1
+for i in range(1, 2):
+    for j in range(2, 5):
+        print(f"I={i + 0.6} J={j + 0.6}")
+        j += 1
+for i in range(1, 2):
+    for j in range(2, 5):
+        print(f"I={i + 0.8} J={j + 0.8}")
+        j += 1
+for i in range(2, 3):
+    for j in range(3, 6):
+        print(f"I={i} J={j}")
+        j += 1
