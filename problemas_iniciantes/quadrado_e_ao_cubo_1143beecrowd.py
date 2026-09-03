a50702fa7cdb8fc29a5c5063cce1b9b1cdb4d03c
miqueias <miqueiasricardobueno@gmail.com>
2026-09-03 17:36:45 -0300

    Exercício bee crowd 1143, quadrado e ao cubo.

---
+++ problemas_iniciantes/quadrado_e_ao_cubo_1143beecrowd.py
@@ -0,0 +1,10 @@
+# Escreva um programa que leia um valor inteiro N (1 < N < 1000).
+# Este N é a quantidade de linhas de saída que serão apresentadas na execução do programa.
+# Entrada
+# O arquivo de entrada contém um número inteiro positivo N.
+# Saída
+# Imprima a saída conforme o exemplo fornecido.
+
+num_linhas = int(input())
+for v in range(1, num_linhas + 1 ):
+    print(v, v**2, v**3)
