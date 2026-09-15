2a3be2c2346ad03c4494a887bf75ee251105fb10
miqueias <miqueiasricardobueno@gmail.com>
2026-09-15 10:03:24 -0300

    Exercício 1158 beecrowd

---
+++ problemas_iniciantes/soma_de_impares_consecutivos_III_1158beecrowd.py
@@ -0,0 +1,24 @@
+# Leia um valor inteiro N que é a quantidade de casos de teste que vem a seguir. 
+# Cada caso de teste consiste de dois inteiros X e Y. 
+# Você deve apresentar a soma de Y ímpares consecutivos a partir de X inclusive o próprio X se ele for ímpar. Por exemplo:
+# para a entrada 4 5, a saída deve ser 45, que é equivalente à: 5 + 7 + 9 + 11 + 13
+# para a entrada 7 4, a saída deve ser 40, que é equivalente à: 7 + 9 + 11 + 13
+# Entrada
+# A primeira linha de entrada é um inteiro N que é a quantidade de casos de teste que vem a seguir. 
+# Cada caso de teste consiste em uma linha contendo dois inteiros X e Y.
+# Saída
+# Imprima a soma dos consecutivos números ímpares a partir do valor X.
+
+quantidade_de_casos_N = int(input())
+x = entradas_x = entradas_y = 0
+for entradas in range(0, quantidade_de_casos_N):
+    entradas_x_y = input().split()
+    entradas_x = int(entradas_x_y[0])
+    entradas_y = int(entradas_x_y[1])
+    if entradas_x % 2 == 0: inicio = entradas_x + 1
+    else: inicio = entradas_x
+    soma  = passo = inicio
+    for total in range(1, entradas_y):      
+        passo += 2
+        soma += passo
+    print(soma)
